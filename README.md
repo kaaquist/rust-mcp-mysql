90f4f9cf606a761606a25c6a25c9655a750aefd1
kaaquist <kasperaaquist@gmail.com>
2025-06-26 13:11:40 +0200

    feat: added dockerfile and corrected the changes that is there now
    Signed-off-by: kaaquist <kasperaaquist@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# MySQL MCP Server - RUST
+> lets see where this will take us all
+
+### Create a container
+using docker
+```
+docker build --platform linux/amd64 -t rust-mcp-mysql:latest -f dockerfile.mysql_mcp_rust .
+```
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,22 @@ use tracing_subscriber::{
 };
 mod utils;
 use utils::counter::Counter;
-const BIND_ADDRESS: &str = "127.0.0.1:9955";
+const BIND_ADDRESS: &str = "0.0.0.0:9955";
 
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let subscriber = tracing_subscriber::Registry::default()
-        .with(
-            // stdout layer, to view everything in the console
-            tracing_subscriber::fmt::layer()
-                .compact()
-                .with_ansi(true)
-        )
+    let subscriber = tracing_subscriber::Registry::default().with(
+        tracing_subscriber::fmt::layer()
+            .compact()
+            .with_ansi(true)
+    )
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "debug".to_string().into()),
         );
     tracing::subscriber::set_global_default(subscriber)?;
+
     let config = SseServerConfig {
         bind: BIND_ADDRESS.parse()?,
         sse_path: "/sse".to_string(),
@@ -38,8 +37,8 @@ async fn main() -> anyhow::Result<()> {
     let listener = tokio::net::TcpListener::bind(sse_server.config.bind).await?;
 
     let ct = sse_server.config.ct.child_token();
-    
     tracing::info!("sse server started!");
+
     let server = axum::serve(listener, router).with_graceful_shutdown(async move {
         ct.cancelled().await;
         tracing::info!("sse server cancelled");
